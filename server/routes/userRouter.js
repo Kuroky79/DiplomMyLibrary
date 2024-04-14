22c5346ff18e9da9423f1111540b6b028b0f058a
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-04-14 15:18:30 +0300

    check work routers

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,7 +4,9 @@ const router = new Router()
 
 router.post('/registration',)
 router.post('/login',)
-router.get('/auth',)
+router.get('/auth',(req,res)=> {
+    res.json({message: 'ALL WORKING!'})
+})
 
 
 module.exports = router
