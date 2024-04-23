1d65268444ad9e747e1619e0eacdc680a27a6c2a
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-04-23 17:02:54 +0300

    Check ApiError

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
-
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,6 +15,8 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
+
+app.use(errorHandler)
 
 const start = async () => {
     try{
