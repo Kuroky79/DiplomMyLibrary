dfd4fc7d7e32b9a0116b5f4c9418de06da8f796c
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-04-24 19:57:55 +0300

    add email and password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,19 @@
 const ApiError = require('../error/apiError')
-
+const bcrypt = require('bcrypt')
+const {User, Basket} = require('../models/models')
 class UserController {
     async registration(req,res){
+        const {email, password, role} = req.body
+        if(!email || !password) {
+            return next(ApiError.badRequest('Некорректный email или password'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate){
+            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
 
     }
     async login(req,res){
