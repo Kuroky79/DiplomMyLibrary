b9ca2858ed8ed3a33bb9085edaed9550d00b9862
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-04-24 19:29:39 +0300

    add info

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,14 +1,24 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Book} = require('../models/models')
+const {Book, BookInfo} = require('../models/models')
 const ApiError = require('../error/apiError')
 class BookController {
     async create(req,res, next){
         try {
-            const {name, brandId, typeId, info} = req.body
+            let {name, brandId, typeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
+
+            if(info){
+                info = JSON.parse(info)
+                info.forEach(i =>
+                    BookInfo.create({
+                        description: i.description,
+                        bookId: book.id
+                    })
+                )
+            }
 
             const book = await Book.create({name, brandId, typeId, info, img: fileName}) // info добавил
             return res.json(book)
@@ -24,21 +34,28 @@ class BookController {
         let offset = page* limit - limit
         let books;
         if (!brandId && !typeId){
-            books = await Book.findAll({limit, offset})
+            books = await Book.findAndCountAll({limit, offset})
         }
         if (brandId && !typeId){
-            books = await Book.findAll({where:{brandId},limit, offset})
+            books = await Book.findAndCountAll({where:{brandId},limit, offset})
         }
         if (!brandId && typeId){
-            books = await Book.findAll({where:{typeId},limit, offset})
+            books = await Book.findAndCountAll({where:{typeId},limit, offset})
         }
         if (brandId && typeId){
-            books = await Book.findAll({where:{brandId, typeId},limit, offset})
+            books = await Book.findAndCountAll({where:{brandId, typeId},limit, offset})
         }
         return res.json(books)
     }
     async getOne(req,res){
-
+        const {id} = req.params
+        const book = await Book.findOne(
+            {
+                where: {id},
+                include: [{model: BookInfo, as: 'info'}]
+            },
+        )
+        return res.json(book)
     }
 
 }
