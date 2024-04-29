5b40d23204fd78a3f591adcdec96854b4564ed8c
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-04-30 01:01:04 +0300

    add modals

--- client/src/components/MyNavbar.js
+++ client/src/components/MyNavbar.js
@@ -1,19 +1,21 @@
 import React, {useContext} from 'react';
 import {Button, Nav, Navbar, NavLink} from "react-bootstrap";
 import {Context} from "../index";
-import {MAIN_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, BOOK_ROUTE, LOGIN_ROUTE, MAIN_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
+import {useNavigate} from "react-router-dom";
 
 const MyNavbar = observer(() => {
     const {user} = useContext(Context)
+    const navigate = useNavigate();
     return (
         <Navbar bg="dark" data-bs-theme="dark" className="justify-content-between">
             <NavLink style={{ color: 'white',fontWeight: 'bold'}} to={MAIN_ROUTE} className="p-2">Library</NavLink> {/* "Library" слева */}
             <Nav className="justify-content-end p-2" style={{ color: 'white' }}> {/* Кнопки справа */}
                 {user.isAuth ?
                     <>
-                        <Button variant={"outline-light"} className="me-2">Админ панель</Button>
-                        <Button variant={"outline-light"}>Войти</Button>
+                        <Button variant={"outline-light"} className="me-2" onClick={() => navigate(ADMIN_ROUTE)}>Админ панель</Button>
+                        <Button variant={"outline-light"} onClick={() => navigate(LOGIN_ROUTE)}>Выйти</Button>
                     </>
                     :
                     <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button, Container} from "react-bootstrap";
+import CreateBrand from "../components/modals/CreateBrand";
+import CreateBook from "../components/modals/CreateBook";
+import CreateType from "../components/modals/CreateType";
 
 const Admin = () => {
+    const [brandVisible, setBrandVisible] = useState(false)
+    const [typeVisible, setTypeVisible] = useState(false)
+    const [bookVisible, setBookVisible] = useState(false)
     return (
-        <div>
-            AdminPage
-        </div>
+        <Container className="d-flex flex-column">
+            <Button
+                variant={"outline-dark"}
+                className="mt-4 p-2" onClick={()=>setTypeVisible(true)}>Добавить тип</Button>
+            <Button
+                variant={"outline-dark"}
+                className="mt-4 p-2" onClick={()=>setBrandVisible(true)}>Добавить бренд</Button>
+            <Button
+                variant={"outline-dark"}
+                className="mt-4 p-2" onClick={()=>setBookVisible(true)}>Добавить книгу</Button>
+            <CreateBrand show={brandVisible} onHide={()=>setBrandVisible(false)}/>
+            <CreateBook show={bookVisible} onHide={()=>setBookVisible(false)}/>
+            <CreateType show={typeVisible} onHide={()=>setTypeVisible(false)}/>
+        </Container>
     );
 };
 
